face9cf8f073ac44e4dd7108e9afa36af5b85c74
syjoy1993 <syjoy1993@gmail.com>
2025-01-17 15:44:51 +0900

    remote

---
+++ src/main/java/ce/argoaws/ArgoAwsApplication.java
@@ -0,0 +1,13 @@
+package ce.argoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ce/argoaws/ArgoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package ce.argoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
